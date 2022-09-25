460b7b43490b3e3eb90e84b043625374e3de49e2
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-09-25 11:59:42 +1000

    testing

--- src/components/columns.jsx
+++ src/components/columns.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import {useState} from 'react'
 
-const ColumnsPage = ({columns, setColumns}) => {
+const ColumnsPage = ({setColumns}) => {
+    const [name, setName] = useState("")
+    const handleAdd = () => {
+        data = {
+            id: name,
+            cell: name,
+            color: "black"
+        }
+    setColumns(prevValue => [...prevValue, data])
+    }
   return (
     <div>
-        {columns.map(column => {
-            return <div style={{border: "1px solid black"}}>
-                <h3>{column.cell}</h3>
-                <h4>{column.color}</h4>
-            </div>
-        })
-    }
+    <input type="text" onChange={e => setName(e.target.value)}/>
+    <button onClick={handleAdd}>ADD</button>
     </div>
   )
 }
